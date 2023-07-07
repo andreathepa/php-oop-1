9f464c46e7e46229f69db0dc7ffa6f7f2596ceb9
andreathepa <andreadepa32@gmail.com>
2023-07-07 16:23:51 +0200

    definita classe movie e istanze

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 
+    class Movie {
+        public $title;
+        public $genre;
+        public $cast;
+        public $release;
+        public $country;
+        public $platform;
+
     
 
+        function __construct($title, $genre, $cast, $release, $country, $platform){
+            $this->title = $title;
+            $this->genre = $genre;
+            $this->cast = $cast;
+            $this->release = $release;
+            $this->country = $country;
+            $this->platform = $platform;
+        }
+    }
+
+    $movie_1 = new movie('Robocop', 'action', 'Peter Weller', '1987', 'USA', 'Netflix');
+    $movie_2 = new movie('Zorro', 'action', 'Antonio banderas', '1999', 'ES', 'Amazon');
+    $movie_3 = new movie('The Departed', 'action', 'Leonardo Di Caprio', '2005', 'USA', 'Disney')
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +44,40 @@
     <body>
         <div class="wrapper">
             <div id="app">
-                
+                <div class="container my-5">
+                    <div class="row">
+                        <div class="col-12">
+                            <ul class="list-unstyled">
+                                <li>Title: <?php echo $movie_1->title; ?></li>
+                                <li>Genre: <?php echo $movie_1->genre; ?></li>
+                                <li>Cast: <?php echo $movie_1->cast; ?></li>
+                                <li>Release: <?php echo $movie_1->release; ?></li>
+                                <li>Country: <?php echo $movie_1->country; ?></li>
+                                <li>Platform: <?php echo $movie_1->platform; ?></li>
+                            </ul>
+                        </div>
+                        <div class="col-12">
+                            <ul class="list-unstyled">
+                                <li>Title: <?php echo $movie_2->title; ?></li>
+                                <li>Genre: <?php echo $movie_2->genre; ?></li>
+                                <li>Cast: <?php echo $movie_2->cast; ?></li>
+                                <li>Release: <?php echo $movie_2->release; ?></li>
+                                <li>Country: <?php echo $movie_2->country; ?></li>
+                                <li>Platform: <?php echo $movie_2->platform; ?></li>
+                            </ul>
+                        </div>
+                        <div class="col-12">
+                            <ul class="list-unstyled">
+                                <li>Title: <?php echo $movie_3->title; ?></li>
+                                <li>Genre: <?php echo $movie_3->genre; ?></li>
+                                <li>Cast: <?php echo $movie_3->cast; ?></li>
+                                <li>Release: <?php echo $movie_3->release; ?></li>
+                                <li>Country: <?php echo $movie_3->country; ?></li>
+                                <li>Platform: <?php echo $movie_3->platform; ?></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
