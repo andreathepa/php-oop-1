2828223c642e3036f9d373767a8c33f005aa68e7
andreathepa <andreadepa32@gmail.com>
2023-07-07 18:12:23 +0200

    function checkGenre, exercise done

--- index.php
+++ index.php
@@ -18,11 +18,20 @@
             $this->country = $country;
             $this->platform = $platform;
         }
+
+        public function checkGenre() {
+        if ($this->genre == 'action' || $this->genre == 'thriller'){
+            return true;
+        }
+        return false;
     }
 
-    $movie_1 = new movie('Robocop', 'action', 'Peter Weller', '1987', 'USA', 'Netflix');
-    $movie_2 = new movie('Zorro', 'action', 'Antonio banderas', '1999', 'ES', 'Amazon');
-    $movie_3 = new movie('The Departed', 'action', 'Leonardo Di Caprio', '2005', 'USA', 'Disney')
+            
+    }
+
+    $movie_1 = new movie('Robocop', 'action', 'Peter Weller', 1987, 'USA', 'Netflix');
+    $movie_2 = new movie('Zorro', 'action', 'Antonio banderas', 1999, 'ES', 'Amazon');
+    $movie_3 = new movie('The Departed', 'thriller', 'Leonardo Di Caprio', 2005, 'USA', 'Disney')
 
     
 ?>
@@ -34,7 +43,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./style.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js" integrity="sha512-uMtXmF28A2Ab/JJO2t/vYhlaa/3ahUOgj1Zf27M5rOo8/+fcTUVH0/E0ll68njmjrLqOBjXM3V9NiPFL5ywWPQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -49,7 +58,7 @@
                         <div class="col-12">
                             <ul class="list-unstyled">
                                 <li>Title: <?php echo $movie_1->title; ?></li>
-                                <li>Genre: <?php echo $movie_1->genre; ?></li>
+                                <li class="<?php if ($movie_1->checkGenre()) echo 'action'; ?>">Genre: <?php echo $movie_1->genre; ?></li>
                                 <li>Cast: <?php echo $movie_1->cast; ?></li>
                                 <li>Release: <?php echo $movie_1->release; ?></li>
                                 <li>Country: <?php echo $movie_1->country; ?></li>
@@ -59,7 +68,7 @@
                         <div class="col-12">
                             <ul class="list-unstyled">
                                 <li>Title: <?php echo $movie_2->title; ?></li>
-                                <li>Genre: <?php echo $movie_2->genre; ?></li>
+                                <li class="<?php if ($movie_1->checkGenre()) echo 'action'; ?>">Genre: <?php echo $movie_2->genre; ?></li>
                                 <li>Cast: <?php echo $movie_2->cast; ?></li>
                                 <li>Release: <?php echo $movie_2->release; ?></li>
                                 <li>Country: <?php echo $movie_2->country; ?></li>
@@ -69,7 +78,7 @@
                         <div class="col-12">
                             <ul class="list-unstyled">
                                 <li>Title: <?php echo $movie_3->title; ?></li>
-                                <li>Genre: <?php echo $movie_3->genre; ?></li>
+                                <li class="<?php if ($movie_1->checkGenre()) echo 'thriller'; ?>">Genre: <?php echo $movie_3->genre; ?></li>
                                 <li>Cast: <?php echo $movie_3->cast; ?></li>
                                 <li>Release: <?php echo $movie_3->release; ?></li>
                                 <li>Country: <?php echo $movie_3->country; ?></li>
